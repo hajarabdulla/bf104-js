079f806c02fe3fd75499287e5e37639c929821c1
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2024-04-02 09:25:59 +0400

    js conditions

---
+++ day 2/app.js
@@ -0,0 +1,118 @@
+// const num = 932;
+
+// const lastNumber = num % 10;
+// const secondNumber = parseInt(num / 10) % 10;
+// const firstNumber = parseInt(num / 100);
+
+// const result = lastNumber * 100 + secondNumber * 10 + firstNumber;
+// console.log(result);
+
+// const num = 3;
+
+// if (num > 3) {
+//   console.log("Number is greater than 3: ", num);
+// } else if (num === 3) {
+//   console.log("Number equals to 3: ", num);
+// } else {
+//   console.log("Number is smaller than 3: ", num);
+// }
+
+// const num = -9;
+
+// if (num > 0) {
+//   console.log("Number is positive", num);
+// } else if (num === 0) {
+//   console.log("Zero");
+// } else {
+//   console.log("Number is negative", num);
+// }
+
+// 1-4 --> initial
+// 5-8 --> average
+// 9-14 --> high
+// let num = -8;
+// if (num >= 1 || num <= 4) {
+//   console.log("inital");
+// } else if (num >= 5 && num <= 8) {
+//   console.log("average");
+// } else if (num >= 9 && num <= 14) {
+//   console.log("high");
+// } else {
+//   console.log("Number is invalid");
+// }
+
+// let num = 3;
+// if (num % 2 == 0) {
+//   console.log("Number is even");
+// } else {
+//   console.log("Number is odd");
+// }
+
+// const dayOfWeek = 1;
+
+// if (dayOfWeek == 1) {
+//   console.log("Monday");
+// } else if (dayOfWeek == 2) {
+//   console.log("Tuesday");
+// } else if (dayOfWeek == 7) {
+//   console.log("Sunday");
+// } else {
+//   console.log("Day is not valid");
+// }
+
+// const dayOfWeek = 5;
+
+// switch (dayOfWeek) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   default:
+//     console.log("Day is not valid");
+// }
+
+// const value = 17;
+// const num1 = 3;
+// const num2 = 4;
+// if (value % num1 == 0 && value % num2 == 0) {
+//   console.log("yes");
+// } else if (value % num1 == 0 || value % num2 == 0) {
+//   console.log("partial");
+// } else {
+//   console.log("no");
+// }
+
+// const number = 22;
+// switch (number) {
+//   case number > 5:
+//     console.log("Bigger than 5");
+//     break;
+//   default:
+//     console.log("Not bigger!");
+// }
+
+const age = 18;
+
+// if (age >= 18) {
+//   console.log("can drive");
+// } else {
+//   console.log("cannot drive");
+// }
+
+const result = age >= 18 ? "Can drive" : "Can not drive";
+console.log(result);
+
+age > 18 ? console.log("Can drive") : console.log("Can not drive");
+
+// ternary ?
