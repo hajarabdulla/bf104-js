72395196ed12fbcea1361d1c265c20b4255c73cf
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2024-04-15 10:01:22 +0400

    slider

---
+++ day 9/app.js
@@ -0,0 +1,11 @@
+const images = document.querySelectorAll(".slide");
+const poster = document.querySelector(".poster");
+
+Array.from(images).map((item) => {
+  item.addEventListener("click", function () {
+    // const imgSrc = item.getAttribute("src");
+    // poster.setAttribute("src", imgSrc);
+
+    poster.src = item.src;
+  });
+});
