ad45aa2c54d3b29b5b412617bc4b8a90cc657edb
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2024-04-15 08:47:55 +0400

    date and bom

---
+++ day 9/app.js
@@ -0,0 +1,29 @@
+// const date = new Date("2022-03-36");
+// console.log(date);
+
+// const date2 = new Date(2022, 3, 36);
+// console.log(date2);
+// date.getMonth();
+
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+
+// window.open("https://classroom.google.com/c/NjY2NDY1NzAyMjk1");
+
+// window.close();
+
+// console.log(window.screen.height);
+// console.log(window.screen.width);
+
+// console.log(window.screen.availHeight);
+// console.log(window.screen.availWidth);
+
+// window.history.forward()
+
+// setTimeout(() => {
+//   console.log("Hello world");
+// }, 3000);
+
+setInterval(() => {
+  document.write("Hello world");
+}, 3000);
